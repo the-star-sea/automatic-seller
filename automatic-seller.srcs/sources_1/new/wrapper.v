f2b43564f27817ec3f805c76db49019deb197b07
Stone <zt980713@gmail.com>
2020-12-21 16:36:16 +0800

    ixx

--- automatic-seller.srcs/sources_1/new/wrapper.v
+++ automatic-seller.srcs/sources_1/new/wrapper.v
@@ -1,23 +1,5 @@
 `timescale 1ns/1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2020/12/12 01:35:27
-// Design Name: 
-// Module Name: wrapper
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 //外壳，最外层模块
 //负责组织所有的输入输出接口，以及内部各模块之间的连接
